08addc3e754d452fec2fc8dae4a3302b3627e9fa
cphuoc281 <cphuoc281@gmail.com>
2024-10-06 19:20:50 +0700

    tách server

--- server/src/main/java/com/web/models/DeleteTokenResponse.java
+++
@@ -1,7 +0,0 @@
-package com.web.models;
-
-public class DeleteTokenResponse extends Response {
-    private String requestId;
-    private String partnerClientId;
-    private Long transId;
-}
--- server/src/main/java/com/web/repository/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.repository;
-
-import com.web.entity.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
