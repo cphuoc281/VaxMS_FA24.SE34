190731cb420367d95aa549c0b4f366b7c3bb2c9a
ducanhlv142 <ducanhlv142@gmail.com>
2024-10-12 16:26:32 +0700

    Remove server folder from develop-client branch

--- server/src/main/java/com/web/repository/NurseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.repository;
-
-import com.web.entity.Nurse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NurseRepository extends JpaRepository<Nurse, Long> {
-}
