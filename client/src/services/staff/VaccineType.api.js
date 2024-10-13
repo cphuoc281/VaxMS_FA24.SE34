6aabec2811326c6d6260402191d7cd3c60cdf38b
ducanhlv142 <ducanhlv142@gmail.com>
2024-10-13 15:29:16 +0700

    Remove client folder from develop-server

--- client/src/services/staff/VaccineType.api.js
+++
@@ -1,9 +0,0 @@
-import { axios } from "./Vaccine.api";
-export class VaccineTypeApi {
-  static vaccineTypes = () => {
-    return axios({
-      method: "GET",
-      url: `/api/vaccine-type/find-all`,
-    });
-  };
-}
